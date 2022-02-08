8d8b3a8707f6987c8a8b28ce5603933881bd3d6f
Till Hoffmann <tillahoffmann@gmail.com>
2022-02-08 18:54:27 -0500

    Ensure sample_posterior returns parameter values.

--- summaries/algorithm.py
+++ summaries/algorithm.py
@@ -6,39 +6,48 @@ class Algorithm:
     def sample_posterior(self, data: np.ndarray, num_samples: int, **kwargs) -> np.ndarray:
         raise NotImplementedError
 
+    @property
+    def num_params(self):
+        raise NotImplementedError
+
 
 class RejectionAlgorithm(Algorithm):
     """
     Simple approximate Bayesian computation rejection sampler with optional feature whitening.
 
     Args:
-        train: Reference table of simulations with shape `(n, p)`, where `n` is the number of
-            simulations and `p` is the number of features.
+        train_features: Reference table of simulated data with shape `(n, p)`, where `n` is the
+            number of simulations and `p` is the number of features.
+        train_params: Reference table of simulated parameter values with shape `(n, q)`, where `n`
+            is the number of simulatiosn and `q` is the number of parameters.
         whiten_features: Whether to whiten the features by multiplying them by the inverse Cholesky
             decomposition of the covariance matrix. This is equivalent to using the Mahalanobis
             distance.
     """
-    def __init__(self, train: np.ndarray, whiten_features: bool = False):
-        # Transform to the right shape.
-        self.train = np.asarray(train)
-        if self.train.ndim == 1:
-            self.train = self.train[:, None]
+    def __init__(self, train_features: np.ndarray, train_params: np.ndarray,
+                 whiten_features: bool = False):
+        # Check shapes.
+        self.train_features = np.asarray(train_features)
+        self.train_params = np.asarray(train_params)
+        assert self.train_features.ndim == 2
+        assert self.train_params.ndim == 2
+        assert self.train_features.shape[0] == self.train_params.shape[0]
 
         if whiten_features:
-            cov = np.atleast_2d(np.cov(self.train, rowvar=False))
+            cov = np.atleast_2d(np.cov(self.train_features, rowvar=False))
             chol = np.linalg.cholesky(cov)
             self.inverse_cholesky = np.linalg.inv(chol)
-            features = self.train @ self.inverse_cholesky
+            features = self.train_features @ self.inverse_cholesky
             # Check that the transformation whitened the features.
             np.testing.assert_allclose(np.cov(features, rowvar=False), np.eye(cov.shape[0]))
         else:
             self.inverse_cholesky = None
-            features = self.train
+            features = self.train_features
 
         self.reference = spatial.KDTree(features)
 
-    def sample_posterior(self, data: np.ndarray, num_samples: int, return_distances: bool = False,
-                         **kwargs) -> np.ndarray:
+    def sample_posterior(self, data: np.ndarray, num_samples: int, return_indices: bool = False,
+                         return_distances: bool = False, **kwargs) -> np.ndarray:
         """
         Draw samples from the reference table that minimise the distance to the data.
 
@@ -50,10 +59,25 @@ class RejectionAlgorithm(Algorithm):
                 reference table.
 
         Returns:
+            y: Posterior samples.
             i: Indices of the samples in `reference`.
             d: Distance of each sample from the data (if `return_distances` is `True`).
         """
+        data = np.asarray(data)
+        assert data.ndim == 1
         if self.inverse_cholesky is not None:
             data = data @ self.inverse_cholesky
-        distance, i = self.reference.query(data, k=num_samples, **kwargs)
-        return (i, distance) if return_distances else i
+        distances, indices = self.reference.query(data, k=num_samples, **kwargs)
+        y = self.train_params[indices]
+        if not (return_indices or return_distances):
+            return y
+        y = [y]
+        if return_indices:
+            y.append(indices)
+        if return_distances:
+            y.append(distances)
+        return tuple(y)
+
+    @property
+    def num_params(self):
+        return self.train_params.shape[1]
--- tests/test_algorithm.py
+++ tests/test_algorithm.py
@@ -3,29 +3,30 @@ import pytest
 import summaries
 
 
-@pytest.mark.parametrize('size, whiten', [
-    (10000, False),
-    ((10000, 1), False),
-    ((10000, 1), True),
-])
-def test_abc(size: tuple, whiten: bool):
+@pytest.mark.parametrize('whiten', [False, True])
+def test_abc(whiten: bool):
     # Generate data from a toy model.
-    mu = np.random.normal(0, 1, size)
-    x = np.random.normal(mu, 1, size)
+    n = 10000
+    mu = np.random.normal(0, 1, (n, 1))
+    x = np.random.normal(mu, 1, (n, 10)).mean(axis=1, keepdims=True)
 
     # Construct the reference table/tree.
-    algorithm = summaries.RejectionAlgorithm(x, whiten)
+    algorithm = summaries.RejectionAlgorithm(x, mu, whiten)
 
     # Sample from the approximate posterior.
     num_samples = 99
-    i, d = algorithm.sample_posterior(x[0], num_samples + 1, True)
+    y1 = algorithm.sample_posterior(x[0], num_samples + 1)
+    y, i, d = algorithm.sample_posterior(x[0], num_samples + 1, True, True)
+    np.testing.assert_array_equal(y, y1)
     # Drop the first sample (because it's what we used to generate the data).
     assert d[0] == 0
     i = i[1:]
+    y = y[1:]
 
     # Verify shape and ensure MSE of the sample is smaller than MSE for the entire reference table.
-    y = mu[i]
     assert i.shape == (num_samples,)
     mse_all = np.square(mu[0] - mu[1:]).mean()
     mse_sample = np.square(mu[0] - y).mean()
     assert mse_all > mse_sample
+
+    assert algorithm.num_params == 1
