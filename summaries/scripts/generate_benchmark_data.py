909d97f5011075d3e67e1db66d9d85936aa00a25
Till Hoffmann <tillahoffmann@gmail.com>
2022-03-03 17:31:00 -0500

    Improve argument handling for benchmark simulation.

--- summaries/scripts/generate_benchmark_data.py
+++ summaries/scripts/generate_benchmark_data.py
@@ -8,9 +8,10 @@ from .. import benchmark
 def __main__(args=None):
     parser = argparse.ArgumentParser()
     parser.add_argument('--seed', type=int, help='seed for the random number generator')
-    parser.add_argument('--num_observations', type=int, help='number of observations per sample')
+    parser.add_argument('--num_observations', type=int, help='number of observations per sample',
+                        default=benchmark.NUM_OBSERVATIONS)
     parser.add_argument('--num_noise_features', type=int, help='number of noise features per '
-                        'sample')
+                        'sample', default=benchmark.NUM_NOISE_FEATURES)
     parser.add_argument('num_samples', type=int, help='number of samples to generate')
     parser.add_argument('output', help='output file path')
     args = parser.parse_args(args)
--- tests/scripts/test_generate_benchmark_data.py
+++ tests/scripts/test_generate_benchmark_data.py
@@ -4,10 +4,13 @@ from unittest import mock
 
 def test_generate_benchmark_data():
     with mock.patch('builtins.open') as open_, mock.patch('pickle.dump') as dump_:
-        generate_benchmark_data.__main__(['--seed=0', '23', 'some_file.pkl'])
+        generate_benchmark_data.__main__(['--seed=0', '--num_observations=17',
+                                          '--num_noise_features=5', '23', 'some_file.pkl'])
 
     open_.assert_called_once_with('some_file.pkl', 'wb')
     dump_.assert_called_once()
     (result, _), _ = dump_.call_args
     assert 'args' in result
     assert len(result['samples']) == 23
+    assert len(result['samples'][0]['x']) == 17
+    assert len(result['samples'][0]['noise']) == 5
